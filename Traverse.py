01bae135622cb289dc03e5e347096a42c9911e62
jugyo <jugyo.org@gmail.com>
2013-02-20 11:08:17 -0800

    improve to start traverse from last directory

--- Traverse.py
+++ Traverse.py
@@ -4,23 +4,30 @@ from glob import iglob
 
 class TraverseCommand(sublime_plugin.WindowCommand):
     def run(self, **kwargs):
+        global last_directory
+
         if kwargs['start'] == "root":
             self.start_directory = "/"
         elif kwargs['start'] == "home":
             self.start_directory = os.path.expanduser("~")
         elif kwargs['start'] == "project":
             self.start_directory = self.window.folders()[0]
+        elif kwargs['start'] == "last":
+            self.start_directory = last_directory
         self.traverse(self.start_directory)
 
     def traverse(self, directory):
-        directory = os.path.abspath(directory)
-        dirs_and_files = self.find_dirs_and_files(directory)
-        if directory != self.start_directory:
+        global last_directory
+
+        last_directory = os.path.abspath(directory)
+
+        dirs_and_files = self.find_dirs_and_files(last_directory)
+        if last_directory != "/":
             dirs_and_files = [".."] + dirs_and_files
 
         def on_done(index):
             if index >= 0:
-                dir_or_file = os.path.join(directory, dirs_and_files[index])
+                dir_or_file = os.path.join(last_directory, dirs_and_files[index])
                 if os.path.isfile(dir_or_file):
                     self.window.open_file(dir_or_file)
                 else:
